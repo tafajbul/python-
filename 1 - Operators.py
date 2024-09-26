2372608eda5b8936acf7db05a802cf5a3d5c3dbf
TAFAJBUL <tafazzulshahid@gmail.com>
2024-09-26 09:42:09 +0530

    Add files via upload

---
+++ 1 - Operators.py
@@ -0,0 +1,25 @@
+## Identity Operators
+# a = [10]
+# b = [10]
+# # a = b
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+## Membership Operators
+# a = [34,56,78]
+# print(34 in a)
+# print(34 not in a)
+
+## Bitwise Operators
+# a = 10
+# b = 3
+# print(a&b)
+# print(a|b)
+# print(a^b)
+
+## Bitshift Operators
+a = 10
+b = 3
+print(a<<b)
+print(a>>b)
