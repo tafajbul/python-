a1cd103927d81ea862f81f880cd098ff1564090d
TAFAJBUL <tafazzulshahid@gmail.com>
2024-09-26 09:41:01 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,22 @@
+# for  i in range(10):
+#     print(i,end=" ")
+
+
+# for i in range (1,11):
+#     print(i, end=" ")
+
+    
+# for i in range (0,51,5):
+#     print(i, end=" ")
+
+
+# revers number in for loop ### (start end step)
+# for i in range(10,0,-1):
+#     print (i)
+
+# Write a Python program to print the numbers from 1 to 10 using a for loop
+# for i in range(1,11):
+#     print(i)
+
+# Question: How would you write a Python program that prints the multiplication table of 5 using
+# for i in range():
